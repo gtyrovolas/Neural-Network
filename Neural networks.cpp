3c9c211a60ee0084c145f0433951133d32bb83ea
Giannis Tyrovolas <johntyro@hotmail.com>
2017-08-26 02:14:06 +0300

    Bug in forward, attempted to correct by using vectors. The sin prediction works well for less than pi/2 and mediocre for values up to pi. For the rest it is problematic.

--- Neural networks.cpp
+++ Neural networks.cpp
@@ -6,16 +6,35 @@
 #include <time.h>
 #include <assert.h>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
 typedef long long ll;
 
 const double e = 2.71828182845904523536028747135266249775724709369995;
+const double pi = 3.14159265359;
 // All these are the infastructure of a matrix
-struct mat{ // Matrix for at most 10x10
+struct mat{ // Matrix for at most 20x5
     ll m, n; // matrix is m x n
-    double M[10][10] = {};
+    vector< vector<double> > M;
+    mat(ll m1, ll n1){
+      m = m1;
+      n = n1;
+
+      M.resize(m1);
+      for(ll i = 0; i < m1; i++){
+        M[i].resize(n1);
+      }
+    }
+    mat(){}
+    void set(ll m1, ll n1){
+      m = m1; n = n1;
+      M.resize(m1);
+      for(ll i = 0; i < m1; i++){
+        M[i].resize(n1);
+      }
+    }
     mat operator +(const mat &A){ // matrix addition
       if(n != A.n || m != A.m)
       {
@@ -23,7 +42,7 @@ struct mat{ // Matrix for at most 10x10
       }
       else
       {
-        mat sol;
+        mat sol(m,n);
         for(ll i = 0; i < m; i++){
           for(ll j = 0; j < n; j++){
             sol.M[i][j] = M[i][j] + A.M[i][j];
@@ -38,9 +57,7 @@ struct mat{ // Matrix for at most 10x10
       if(n != A.m){
         throw invalid_argument( "Multiplication failed due to incompatible matrices" );
       }
-      mat sol;
-      sol.m = m;
-      sol.n = A.n;
+      mat sol(m, A.n);
       for(ll i = 0; i < m; i++){
         for(ll j = 0; j < A.n; j++){
           double sum = 0;
@@ -60,18 +77,20 @@ struct mat{ // Matrix for at most 10x10
       }
       else
       {
-        mat sol;
+        mat sol(m,n);
         for(ll i = 0; i < m; i++){
           for(ll j = 0; j < n; j++){
             sol.M[i][j] = M[i][j] - A.M[i][j];
           }
         }
-        sol.m = m;
-        sol.n = n;
         return sol;
       }
     }
     mat operator =(const mat &A){ // equals
+      M.resize(A.m);
+      for(ll i = 0; i < m; i++){
+        M[i].resize(A.n);
+      }
       m = A.m;
       n = A.n;
       for(ll i = 0; i < m; i++){
@@ -82,9 +101,7 @@ struct mat{ // Matrix for at most 10x10
     }
 };
 mat T(mat A){
-    mat sol;
-    sol.m = A.n;
-    sol.n = A.m;
+    mat sol(A.n, A.m);
 
     for(ll i = 0;i < A.m; i++){
       for(ll j = 0; j < A.n; j++){
@@ -107,7 +124,7 @@ void outD(mat A){ // ouput Dimensions
 }
 mat multiply(mat A, mat B){ // scalar multiplication of two matrices
     if(A.m != B.m || A.n != B.n) throw invalid_argument("Scalar Multiplication of Matrices Failed\n");
-    mat sol;
+    mat sol(A.n,A.m);
     sol.n = A.n;
     sol.m = A.m;
     for(ll i = 0; i < A.m; i++){
@@ -143,16 +160,18 @@ mat scalMult(mat A, double sc){ // multiply a matrix A by a scalar
 
 
 // setting HyperParameters
-ll inputSize = 2; // number of input neurons
+ll inputSize = 1; // number of input neurons
 ll outputSize = 1; // number of output neurons
-ll hiddenLayer = 3; // number of neurons in the hidden layers
-ll testSize = 5;  // number of testing data
-ll dataM = 3;  // number of cases, also one dimension of the input matrix
-ll dataN = 2;  // number of input neurons
+ll hiddenLayer = 1; // number of neurons in the hidden layers
+ll testSize = 0;  // number of testing data
+ll dataM = 5;  // number of cases, also one dimension of the input matrix
+ll dataN = inputSize;  // number of input neurons
 
-mat X; // input
-mat Y; // result
-mat test; // testing data
+
+mat X(dataM,dataN); // input
+mat Y(dataM,outputSize); // result
+mat testX; // testing input data
+mat testY; // testing output data
 
 mat W[10]; // matrix that contains the weights of the synapses
 mat Z[5];  // matrix that contains the raw version of the data
@@ -163,8 +182,7 @@ double sigmoid(double z){ // sigmoid of a number
     return (double)1/(1 + pow(e, -z));
 }
 mat sigmoid(mat z){ // making the sigmoid of each item in a matrix
-    mat sol;
-    sol.m = z.m; sol.n = z.n;
+    mat sol(z.m, z.n);
     for(ll i = 0; i < z.m; i++){
       for(ll j = 0; j < z.n; j++){
         sol.M[i][j] = (double)1/(1 + pow(e, -(z.M[i][j])));
@@ -173,31 +191,64 @@ mat sigmoid(mat z){ // making the sigmoid of each item in a matrix
     return sol;
 }
 
+double norm(double in){ // function to normalise complex input
+    return in;
+}
+
+double denorm(double in){ // function to denormalise complex input
+    return in;
+
+}
 void init(){ // initialising the training
+
+    // initialising input
     X.m = dataM;
     X.n = dataN;
-    X.M[0][0] = 3.0/10; X.M[0][1] = 10.0/10;
-    X.M[1][0] = 5.0/10; X.M[1][1] = 2.0/10;
-    X.M[2][0] = 10.0/10; X.M[2][1] = 4.0/10;
+    for(ll i = 0; i < dataM; i++){
+      X.M[i][0] = (pi*i/2)/ (dataM - 1);
+    }
+    // initialising output
     Y.m = dataM;
-    Y.n = 1;
+    Y.n = outputSize;
 
-    Y.M[0][0] = 0.75;
-    Y.M[1][0] = 0.82;
-    Y.M[2][0] = 0.93;
+    for(ll i = 0; i < dataM; i++){
+      Y.M[i][0] = norm(sin(X.M[i][0]));
+    }
 
-
+    // initialising weights
+    W[1].set(inputSize,hiddenLayer);
     W[1].m = inputSize;
     W[1].n = hiddenLayer;
     genMat(W[1], -2, 2);
+    W[2].set(hiddenLayer,outputSize);
     W[2].m = hiddenLayer;
     W[2].n = outputSize;
-    genMat(W[2], -3, 3);
+    genMat(W[2], -2, 2);
+
+    // initialising testing data
+
+    testX.set(testSize,X.n);
+    testX.m = testSize;
+    testX.n = X.n;
+
+    testY.set(testSize, Y.n);
+    testY.m = testSize;
+    testY.n = Y.n;
+
+
+    for(int i = 0; i < testSize; i++){
+      testX.M[i][0] = fRand(0, pi);
+      testY.M[i][0] = norm(sin(testX.M[i][0]));
+    }
+
 }
 
 mat forward(mat X){  // forward propagation
-    mat yHat;
+    mat yHat(dataM, outputSize);
+    outD(X);
+    outD(W[1]);
     Z[2] = X * W[1];
+    cout << "tes2t "<<endl;
     A[2] = sigmoid(Z[2]);
     Z[3] = A[2] * W[2];
     yHat = sigmoid(Z[3]);
@@ -225,7 +276,7 @@ double sigmoidPrime(double z){ // Returns the derivative of the sigmoid function
 }
 
 mat sigmoidPrime(mat z){ // returns the derivative of each element in a matrix
-    mat sol;
+    mat sol(z.m, z.n);
     sol.n = z.n;
     sol.m = z.m;
     for(ll i = 0; i < sol.m; i++){
@@ -262,51 +313,18 @@ void costFPrime(mat X, mat y, mat &yHat, mat &dJdW1, mat &dJdW2){
     dJdW1 = T(X) * delta2;
 }
 
-void regulate(mat X, mat y, mat &yHat, double rate){
-    mat dJdW1, dJdW2;
-    costFPrime(X, y, yHat, dJdW1, dJdW2);
-    W[1] = W[1] - scalMult(dJdW1, rate);
-    W[2] = W[2] - scalMult(dJdW2, rate);
-}
-
-ll train(mat X, mat y, ll trials, double rate){
-    mat dJdW1, dJdW2;
-    mat best;
-    ll cnt = 0;
-    mat yHat;
-    ll id;
-    double mn = 100000;
-    for(ll i = 0; i < trials; i++){
-      regulate(X, y, yHat,rate);
-   //   out(yHat);
-      if(cost2(yHat, y) < mn){
-        mn = cost2(yHat, y);
-        id = i;
-      }
-    }
-    return id;
-   // cout << cost(yHat, y) << " after " << cnt << endl;
-   // out(yHat);
-}
-
-
-// check with numerical gradient
-
-void getGradients(mat &dJdW1, mat &dJdW2){ // function to get the Gradients computed by costFPrime
-    mat yHat;
-    costFPrime(X, Y, yHat, dJdW1, dJdW2);
-}
-
-
 void computeNumericalGradient(mat X, mat Y, mat &numdW1, mat &numdW2){ // Numerical gradient Descent
     mat dJdW1, dJdW2;
-    getGradients(dJdW1, dJdW2);
-    double eps = 0.0001;
-    numdW1.n = dJdW1.n; numdW1.m = dJdW1.m;
-    numdW2.n = dJdW2.n; numdW2.m = dJdW2.m;
+    double eps = 0.00001;
+    numdW1.set(W[1].n, W[1].m);
+    numdW2.set(W[2].n, W[2].m);
+
+    numdW1.n = W[1].n; numdW1.m = W[1].m;
+    numdW2.n = W[2].n; numdW2.m = W[2].m;
 
     for(ll i = 0; i < numdW2.m; i++){
       for(ll j = 0; j < numdW2.n; j++){
+
         W[2].M[i][j] += eps;
         double loss2 = cost(X, Y);
 
@@ -333,34 +351,110 @@ void computeNumericalGradient(mat X, mat Y, mat &numdW1, mat &numdW2){ // Numeri
 }
 
 
+void regulate(mat X, mat y, mat &yHat, double rate){
+    mat dJdW1, dJdW2;
+    computeNumericalGradient(X, y, dJdW1, dJdW2);
+
+    cout << "1 "<< endl;
+    mat prevW [10] = {};
+    prevW[1] = W[1];
+    prevW[2] = W[2];
+
+    cout << "one " << endl;
+    yHat = forward(X);
+
+    double prevC;
+    do {
+      cout << "test "<< endl;
+      prevC = cost2(y, yHat);
+      W[1] = W[1] - scalMult(dJdW1, rate);
+      W[2] = W[2] - scalMult(dJdW2, rate);
+      yHat = forward(X);
+    }while(cost2(yHat, y) < prevC);
+}
+
+ll train(mat X, mat y, ll trials, double rate){
+    mat dJdW1, dJdW2;
+    mat best;
+    ll cnt = 0;
+    mat yHat;
+    ll id;
+    double mn = 100000;
+    for(ll i = 0; i < trials; i++){
+      regulate(X, y, yHat,rate);
+   //   out(yHat);
+      if(cost2(yHat, y) < mn){
+        mn = cost2(yHat, y);
+        id = i;
+      }
+      if(i % 100 == 0){
+        cout << "Trial " << i << " cost is " << cost2(yHat, y) << endl;
+      }
+    }
+    return id;
+   // cout << cost(yHat, y) << " after " << cnt << endl;
+   // out(yHat);
+}
+
+
+// check with numerical gradient
+
+void getGradients(mat &dJdW1, mat &dJdW2){ // function to get the Gradients computed by costFPrime
+    mat yHat;
+    costFPrime(X, Y, yHat, dJdW1, dJdW2);
+}
+
+double test(mat & yHat){
+
+    yHat = forward(testX);
+    return cost2(yHat, testY);
+
+}
+
+
 
 int main(){
     srand(time(NULL));
-    init();
 
     mat dJdW1, dJdW2;
     mat numdW1, numdW2;
-    cout << train(X, Y, 10000, 1) << endl;
+    init();
+    train(X, Y, 20000, 10);
     cout << "yHat\n";
-    out(forward(X));
+  //  out(forward(X));
     cout  << " vs Y" << endl;
-    out(Y);
-    cout << cost2(Y,forward(X)) << endl;
+  //  out(Y);
+    cout << "cost for Y " << cost2(Y,forward(X)) << endl;
 
     cout << "Weights \n";
     out(W[1]);
     out(W[2]);
+    mat yHat;
 
+    for(ll i = 0; i < 31; i++){
+      mat t1;
+      t1.n = 1; t1.m = 1;
+      t1.M[0][0] =(double) (i) / 10;
+      mat res = forward(t1);
+      cout << "For: " << t1.M[0][0] << " calculated " << denorm(res.M[0][0]) << " correct " << sin(t1.M[0][0]) << endl;
+    }
 
-    mat X2;
-    X2.n = 2;
-    X2.m = 4;
-    X2.M[0][0] = 0; X2.M[0][1] = 0;
-    X2.M[1][0] = 0; X2.M[1][1] = 1;
-    X2.M[2][0] = 1; X2.M[2][1] = 0;
-    X2.M[3][0] = 1; X2.M[3][1] = 1;
-    cout << "Results of X2" << endl;
-    out(forward(X2));
+    cout << "Time for interactive testing!!!\n";
+    for(ll i = 0; i < 100; i++){
+      cout << "Input a number and the program will calculate its sine\n";
+      double t;
+      cin >> t;
+      mat t1;
+      t1.n = 1;
+      t1.m = 1;
+      t1.M[0][0] = t;
+      mat res = forward(t1);
+      double fin = denorm(res.M[0][0]);
+      cout << "The program found " << fin << " as the answer\n";
+      cout << "The correct answer is " << sin(t) << endl;
+      cout << "The square of the difference is " << (fin - sin(t)) *(fin - sin(t)) << endl << endl << endl;
+    }
+
 }
 
 
